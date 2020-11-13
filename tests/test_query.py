87d4f38eae450830161abb1787e67e3372019a04
Daniel Kopecký <kopecky.d@gmail.com>
2020-11-13 15:42:21 +0100

    tests

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,9 +1,14 @@
-from nr_oai_pmh_harvester.query import find_in_json_list
+from nr_oai_pmh_harvester.query import find_in_json_list, get_query_by_slug
 
 
 def test_query(app, db):
-    sqlalchemy_query = find_in_json_list("institutions", "formerNames", "Vysoká škola zemědělská v Brně")
+    sqlalchemy_query = find_in_json_list("institutions", "formerNames",
+                                         "Vysoká škola zemědělská v Brně")
     term = sqlalchemy_query.all()
     assert term[0].slug == '62156489'
 
 
+def test_get_query_by_slug(app, db):
+    query = get_query_by_slug("subjects", "PSH120")
+    terms = query.all()
+    print(query, terms)
--- tests/test_transform.py
+++ tests/test_transform.py
@@ -1,22 +1,38 @@
 import pathlib
 from pprint import pprint
 
+import pytest
 from lxml import etree
 from oarepo_oai_pmh_harvester.transformer import OAITransformer
 
 from nr_oai_pmh_harvester.parser import marcxml_parser
+from nr_oai_pmh_harvester.rules.nusl.field001 import control_number
 from nr_oai_pmh_harvester.rules.nusl.field035 import original_record_oai
 from nr_oai_pmh_harvester.rules.nusl.field04107 import language
 from nr_oai_pmh_harvester.rules.nusl.field046__k import date_issued
 from nr_oai_pmh_harvester.rules.nusl.field24500 import title
+from nr_oai_pmh_harvester.rules.nusl.field300 import extent
+from nr_oai_pmh_harvester.rules.nusl.field336__a import certified_methodologies
+from nr_oai_pmh_harvester.rules.nusl.field502__c import degree_grantor
+from nr_oai_pmh_harvester.rules.nusl.field520 import abstract
+from nr_oai_pmh_harvester.rules.nusl.field586 import defended
+from nr_oai_pmh_harvester.rules.nusl.field650_7 import subject
+from nr_oai_pmh_harvester.rules.nusl.field653 import keyword
+from nr_oai_pmh_harvester.rules.nusl.field7102 import degree_grantor_2
+from nr_oai_pmh_harvester.rules.nusl.field720 import people
+from nr_oai_pmh_harvester.rules.nusl.field85640 import original_record_id
+from nr_oai_pmh_harvester.rules.nusl.field85642u import external_location
+from nr_oai_pmh_harvester.rules.nusl.field909COo import nusl_oai
+from nr_oai_pmh_harvester.rules.nusl.field980__a import resource_type
+from nr_oai_pmh_harvester.rules.nusl.field996 import accessibility
+from nr_oai_pmh_harvester.rules.nusl.field998 import provider
 from nr_oai_pmh_harvester.utils import transform_to_dict
 
-from nr_oai_pmh_harvester.rules.nusl.field001 import control_number
 
-
-def test_uk_bachelor_thesis(app, db):
+@pytest.mark.parametrize("file_name", ["416174", "253605"])
+def test_uk_bachelor_thesis(app, db, file_name):
     this_directory = pathlib.Path(__file__).parent.absolute()
-    response_path = this_directory / "data" / "416174.xml"
+    response_path = this_directory / "data" / f"{file_name}.xml"
     with open(str(response_path), "r") as f:
         tree = etree.parse(f)
         root = tree.getroot()
@@ -39,6 +55,55 @@ def test_uk_bachelor_thesis(app, db):
         "/24500": {
             "pre": title
         },
+        "/502__/c": {
+            "pre": degree_grantor
+        },
+        "/520__": {
+            "pre": abstract
+        },
+        "/586__": {
+            "pre": defended
+        },
+        "/653__": {
+            "pre": keyword
+        },
+        "/7102_": {
+            "pre": degree_grantor_2
+        },
+        "/720__": {
+            "pre": people
+        },
+        "/85640": {
+            "pre": original_record_id
+        },
+        "/909CO": {
+            "pre": nusl_oai
+        },
+        "/980__/a": {
+            "pre": resource_type
+        },
+        "/996__": {
+            "pre": accessibility
+        },
+        "/998__/a": {
+            "pre": provider
+        },
+        "/85642/u": {
+            "pre": external_location
+        },
+        "/650_7": {
+            "pre": subject
+        },
+        "/300__/a": {
+            "pre": extent
+        },
+        "/336__/a": {
+            "pre": certified_methodologies
+        },
     }
-    transformer = OAITransformer(rules=rules, unhandled_paths=set(["/leader", "/005", "/008"]))
-    transformer.transform(parsed)
+    transformer = OAITransformer(rules=rules, unhandled_paths=set(
+        ["/leader", "/005", "/008", '/502__/b', '/502__/d', "/6530_", "/909CO/p", "/8560_",
+         "/85642/z", "/8564_"]))
+    transformed = transformer.transform(parsed)
+    print(10 * "\n", "RECORD")
+    pprint(transformed)
